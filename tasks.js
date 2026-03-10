c1ed68648260824d235eebe3d1ad9873c713e03e
SadiFrontend <yachiter1112@gmail.com>
2026-03-10 17:57:16 +0500

    ye

--- tasks.js
+++ tasks.js
@@ -1,30 +1,18 @@
-console.log("1:", Boolean(0));
-console.log("2:", Boolean("0"));
-console.log("3:", Boolean(""));
-console.log("4:", Boolean(" "));
-console.log("5:", Boolean(null));
-console.log("6:", Boolean(undefined));
-console.log("7:", Boolean(NaN));
-console.log("8:", Boolean(100));
-console.log("9:", Boolean("JavaScript"));
-console.log("10:", Boolean(-1));
-
-
-
-console.log("11:", true && false);
-console.log("12:", true && "Salom");
-console.log("13:", 0 && "JavaScript");
-console.log("14:", "Hello" && 100);
-console.log("15:", null && "JS");
-
-
-
-console.log("16:", true || false);
-console.log("17:", false || "Salom");
-console.log("18:", 0 || "JavaScript");
-console.log("19:", null || 100);
-console.log("20:", undefined || "Frontend");
-
-
-
-console.log("Bonus:", "" || 0 || null || "JS" || 5);
+const main = document.getElementById('main');
+const keyEl = document.getElementById('key');
+const codeEl = document.getElementById('code');
+const keyCodeEl = document.getElementById('keyCode');
+const ctrlEl = document.getElementById('ctrl');
+const altEl = document.getElementById('alt');
+const shiftEl = document.getElementById('shift');
+const metaEl = document.getElementById('Mod');
+window.addEventListener('keydown', (e) => {
+  main.textContent = e.code;
+  keyEl.textContent = e.key;
+  codeEl.textContent = e.code;
+  keyCodeEl.textContent = e.keyCode;
+  ctrlEl.textContent = e.ctrlKey;
+  altEl.textContent = e.altKey;
+  shiftEl.textContent = e.shiftKey;
+  metaEl.textContent = e.metaKey;
+});
