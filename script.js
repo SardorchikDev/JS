3b102e0351cca30f0ad103de6e7088db44c57aff
Sardorchikdev <yachiter1112@gmail.com>
2026-05-07 18:35:52 +0500

    axios

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ const apiKey = '46351da790226c653537b9628dc20463';
 
 function getWeather(city) {
   const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
-  const xmlr = new XMLHttpRequest();
-  xmlr.open('GET', api, true);
-  xmlr.onreadystatechange = function() {
-    if (xmlr.readyState === 4) {
-      if (xmlr.status === 200) {
-        const data = JSON.parse(xmlr.responseText);
-        document.getElementById('SHAHAR').textContent = 'Shahar: ' + data.name + ', ' + data.sys.country;
-        document.getElementById('TEMPERATURA').textContent = 'temperatura: ' + data.main.temp + '°C';
-      } else {
-        console.error('status mana : ', xmlr.status);
-      }
-    }
-  };
-  xmlr.send();
+  axios.get(api)
+    .then(response => {
+      const data = response.data;
+      document.getElementById('SHAHAR').textContent = 'City: ' + data.name + ', ' + data.sys.country;
+      document.getElementById('TEMPERATURA').textContent = 'Temperature: ' + data.main.temp + '°C';
+    })
 }
 
 document.getElementById('KNOPKA').addEventListener('click', () => {
