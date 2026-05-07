60e2adc5605267c34f51337b9d2f674a1f4db659
Sardorchikdev <yachiter1112@gmail.com>
2026-05-07 18:48:09 +0500

    axios

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-const request = new XMLHttpRequest();
-request.addEventListener("load", function() {
-  if (request.readyState === 4 && request.status === 200) {
-    const data = JSON.parse(this.responseText);
+axios.get('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    const data = response.data;
     const ekranchiqargich = document.getElementById("posts");
-    data.forEach((todo) => {
-      const { title, id, completed } = todo;
+    data.forEach((users) => {
+      const { id, name, username, email } = users;
       const ekranchiqar = document.createElement("div");
-      ekranchiqar.className = "post " + (completed ? "completed" : "not-completed");
+      ekranchiqar.className = "user";
       ekranchiqar.innerHTML = `
 
-        <h2>${title}</h2>
+        <h2>${name}</h2>
         <p>ID: ${id}</p>
-        <p>Completed: ${completed ? "true" : "false"}</p>
+        <p>Username: ${username}</p>
+        <p>Email: ${email}</p>
 
-        
+
       `;
       ekranchiqargich.appendChild(ekranchiqar);
     });
-  }
-});
-request.open("GET", "https://jsonplaceholder.typicode.com/todos");
-request.send();
+  });
